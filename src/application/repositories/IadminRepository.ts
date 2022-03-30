4303991c190f31975d49b2ed9d3b5ad69c7e1551
Victor Silva <victor.silvaPC@hotmail.com>
2022-03-30 19:25:08 -0300

    feat: updated

--- src/application/repositories/IadminRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Admin } from "../../domain/entities/admin"
-
-export interface AdminRepository {
-  login(email: string, password: string): Promise<Admin>; // For while I'll keep any signature
-  logout(): Promise<any>;
-}
