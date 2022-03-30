e4ae83e74450854004313f2d624f4d30f9ffadec
Victor Silva <victor.silvaPC@hotmail.com>
2022-03-30 19:21:42 -0300

    feat: using YUP to create initial schema and validate the request fields

---
+++ src/utils/validation.ts
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+import { CardProps } from "../domain/entities/card";
+
+// Generate Validation
+
+const generateValidation = async (data: CardProps) => {
+  const schema = yup.object().shape({
+    title: yup.string().min(3).required(),
+    number_of_cards: yup.number().min(1).required(),
+    unit_price: yup.number().min(0.5).required(),
+    min: yup.number().required(),
+    max: yup.number().min(1).required(),
+    amount_random_number: yup.number().min(1).required(),
+    client_id: yup.string().uuid().required(),
+    date_sort: yup.date().required(),
+  });
+
+  await schema.validate(data);
+};
+
+export { generateValidation };
