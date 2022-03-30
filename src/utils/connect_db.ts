9e564c9904057f0423483eee396d25b292b39311
Victor Silva <victor.silvaPC@hotmail.com>
2022-03-30 19:19:44 -0300

    fix: environment variable naming has been changed

--- src/utils/connect_db.ts
+++ src/utils/connect_db.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config();
 
 const URL = process.env.SUPABASE_URL_API as string;
-const KEY = process.env.SUPABASE_ANON_KEY as string;
+const KEY = process.env.SUPABASE_SERVICE_KEY as string;
 
 const supabase = createClient(URL, KEY);
 
