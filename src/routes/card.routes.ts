7a27e6307f81e98a627d6736abf0e07732ee898e
Victor Silva <victor.silvaPC@hotmail.com>
2022-03-30 19:22:10 -0300

    feat: routes functionalities added

--- src/routes/card.routes.ts
+++ src/routes/card.routes.ts
@@ -3,7 +3,7 @@ import { CardController } from "../application/useCases/Card/cardController";
 import { GenerateCardUseCase } from "../application/useCases/Card/generateCardUseCase";
 import { ReadCardUseCase } from "../application/useCases/Card/readCardUseCase";
 import { DeleteCardUseCase } from "../application/useCases/Card/deleteCardUseCase";
-
+import { generateValidation } from "../utils/validation";
 // routes
 const cardRoutes = Router();
 
@@ -11,42 +11,66 @@ const cardRoutes = Router();
 const cardController = new CardController();
 
 // POST
-cardRoutes.post("/", async (req, res) => {
+cardRoutes.post("/generate", async (req, res) => {
   const generateCard = new GenerateCardUseCase(cardController);
 
   try {
-    const result = await generateCard.execute(req.body);
+    // Validating the Data before proceduring with the logic code.
+    await generateValidation(req.body);
 
-    return res.json({result: result})
+    // The number of cards * amount of random number must be less than the max - min result
+    if (
+      +req.body.amount_random_number * +req.body.number_of_cards >
+      +req.body.max - +req.body.min
+    ) {
+      return res
+        .status(400)
+        .send(
+          "The range of numbers and amount of card doesn't match. The max - min must be lower than number of card to generate"
+        );
+    }
+
+    const { data, error } = await generateCard.execute(req.body);
+
+    if (error) return res.status(400).send(error);
+
+    return res.status(200).send(data);
   } catch (error: any) {
-    console.log(error);
-    return res.json({error: error})
+    return res.status(400).send(error.message);
   }
 });
 
 // GET ALL Cards
-cardRoutes.get("/", async (_, res) => {
+cardRoutes.get("/", async (req, res) => {
   const readCards = new ReadCardUseCase(cardController);
 
+  let user_uuid = req.header("user-uuid");
+
   try {
-    const result = await readCards.execute();
-    return res.json({result: result})
+    const { data, error } = await readCards.execute(user_uuid || "");
+
+    if (error) return res.status(400).send(error);
+
+    return res.status(200).send(data);
   } catch (error: any) {
-    return res.json({error: error})
+    return res.status(400).send(error.message);
   }
 });
 
 //DELETE CARD
-cardRoutes.delete("/:id", async (req, res) => {
+cardRoutes.delete("/delete/:card_id", async (req, res) => {
   const deleteCard = new DeleteCardUseCase(cardController);
 
-  const { id } = req.params;
+  const { card_id } = req.params;
 
   try {
-    const result = await deleteCard.execute(id);
-    return res.json({result: result})
+    const { data, error } = await deleteCard.execute(card_id);
+
+    if (error) return res.status(400).send(error);
+
+    return res.status(200).send(data);
   } catch (error: any) {
-    return res.json({error: error})
+    return res.status(400).send(error.message);
   }
 });
 
