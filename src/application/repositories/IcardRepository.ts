60d509f5210ea9fa5c8e272b9e8c954b2929f826
Victor Silva <victor.silvaPC@hotmail.com>
2022-04-02 21:15:20 -0300

    feat: added update method to Card Interface

--- src/application/repositories/IcardRepository.ts
+++ src/application/repositories/IcardRepository.ts
@@ -3,5 +3,6 @@ import { CardProps } from "../../domain/entities/card";
 export interface CardRepository {
   generate(card: CardProps): Promise<any>;
   read(client_id: string): Promise<any>;
+  update(card_id: string, new_status: string): Promise<any>;
   delete(card_id: string): Promise<any>;
 }
