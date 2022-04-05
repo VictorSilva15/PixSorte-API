24402410b73bc9fc0513752b1fd50ba2eb4d2590
Victor Hugo da Silva <victor.silva@ax4b.com>
2022-04-05 01:29:29 -0300

    feat: user login/register validation fields

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,9 +1,10 @@
 import * as yup from "yup";
 import { CardProps } from "../domain/entities/card";
+import { UserProps } from "../domain/entities/user";
 
-// Generate Validation
+// Generate Card Validation
 
-const generateValidation = async (data: CardProps) => {
+const generateCardValidation = async (data: CardProps) => {
   const schema = yup.object().shape({
     title: yup.string().min(3).required(),
     number_of_cards: yup.number().min(1).required(),
@@ -18,12 +19,12 @@ const generateValidation = async (data: CardProps) => {
   await schema.validate(data);
 };
 
-// Update Validation
+// Update Card Validation
 type UpdateStatusCardRequestProps = {
   new_status: string;
 };
 
-const updateValidation = async (data: UpdateStatusCardRequestProps) => {
+const updateCardValidation = async (data: UpdateStatusCardRequestProps) => {
   const schema = yup.object().shape({
     new_status: yup.string().min(3).required(),
     value_sorted: yup.number(),
@@ -32,4 +33,28 @@ const updateValidation = async (data: UpdateStatusCardRequestProps) => {
   await schema.validate(data);
 };
 
-export { generateValidation, updateValidation };
+
+// User Register Validation
+const userLoginValidation = async (data: UserProps) => {
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(6).max(100).required(),
+  })
+
+  return schema.validate(data);
+}
+
+// User Register Validation
+const userRegisterValidation = async (data: UserProps) => {
+  const schema = yup.object().shape({
+    user_name: yup.string().min(3).max(250).required(),
+    enterprise_name: yup.string().min(3).max(250).required(),
+    email: yup.string().email().required(),
+    password: yup.string().min(6).max(100).required(),
+  })
+
+  return schema.validate(data);
+}
+
+// Exporting All validations
+export { generateCardValidation, updateCardValidation, userLoginValidation, userRegisterValidation };
