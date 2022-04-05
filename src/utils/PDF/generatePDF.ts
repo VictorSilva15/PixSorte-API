e5ef7ef8b80e30976cf9bdc982e39c487577006d
Victor Silva <victor.silvaPC@hotmail.com>
2022-04-04 22:54:02 -0300

    feat: new Changes

---
+++ src/utils/PDF/generatePDF.ts
@@ -0,0 +1,34 @@
+import { CardProps } from "../../domain/entities/card";
+
+// Configuration do generate PDF
+import ejs from "ejs";
+import pdf from "html-pdf";
+import path from "path";
+
+export async function generatePDF(card: CardProps) {
+  const model = path.resolve(__dirname, "model.ejs");
+  let result: any;
+
+  // Generating PDF
+  try {
+    ejs.renderFile(model, card, (err, html) => {
+      if (err) {
+        throw new Error(`Something wrong happend`);
+      } else {
+        pdf
+          .create(html, {} /* options */)
+          .toFile(__dirname + "/card.pdf", (err, pdf) => {
+            if (err) {
+              throw new Error(`Impossible create the PDF`);
+            } else {
+              console.log(pdf);
+            }
+          });
+      }
+    });
+
+    return result;
+  } catch (err: any) {
+    return { error: err?.message };
+  }
+}
